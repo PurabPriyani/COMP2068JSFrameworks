293213cefed8ba32325df0e2e460f816c21d4a6f
PurabPriyani <purabpriyani@gmail.com>
2025-11-18 21:02:57 -0500

    making changes for deployment

--- Paypal/backend/index.js
+++ Paypal/backend/index.js
@@ -15,4 +15,5 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/api/transactions', require('./routes/transactionRoutes'));
 
-app.listen(3000, () => console.log('Server running on 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port", PORT));
